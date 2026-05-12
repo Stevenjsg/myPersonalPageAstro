3168925dac960278bdbe8c9b2d31d0a28321218b
Steven J. Silva Gomez <stevenibz-96@hotmail.com>
2026-05-12 23:27:24 +0200

    fix: Corregir la ruta base en la configuración de Astro y eliminar enlace a CSS en index.astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   integrations: [react()],
-  base: "cv/",
+  base: "/cv",
   vite: {
     plugins: [tailwindcss()],
   },
